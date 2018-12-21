feb8de61a374c104f2d4635ae65276a923f9c0fc
Luise Schneider <luise.schneider@berlin.de>
2018-12-21 18:07:25 +0100

    about done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/ueber", (req, res, next) => {
+  res.render("ueber");
+});
+
+router.get("/fotograf", (req, res, next) => {
+  res.render("fotograf");
 });
 
 module.exports = router;
